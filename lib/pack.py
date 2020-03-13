bb1e040cf7d22e0852fd024be3d361cf673d92d8
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-03-13 16:43:32 -0700

    Pack shuffle and tests added

--- lib/pack.py
+++ lib/pack.py
@@ -1,20 +1,51 @@
+import random
 
 class Pack:
   """
   Set of standard 52 card decks used to play Blackjack.
   """
 
+  _DECKS = 6
+  _DECK_SIZE = 52
+
   def __init__(self):
     """
     Pack constructor.
     """
-    pass
+    
+    self.cards = []
+
+    for _ in range(Pack._DECKS):
+      deck = _Deck()
+      for card in deck.cards:
+        self.cards += [card]
+
+  def shuffle(self, _random = None):
+    """
+    Shuffle a pack of six card decks.
+
+    In:
+    random (function): random
+    """
+
+    for i, card in enumerate(self.cards):
+      swap_idx = -1
+      if _random:
+        swap_idx = _random(0, Pack._DECK_SIZE * Pack._DECKS - 1)
+      else:
+        swap_idx = random.randint(0, Pack._DECK_SIZE * Pack._DECKS - 1)
+
+      swap_card = self.cards[swap_idx]
+      self.cards[swap_idx] = card
+      self.cards[i] = swap_card
 
 
 class _Deck:
   """
   Standard 52 card deck.
   """
+
+  _CARDS = 52
 
   def __init__(self):
     """
@@ -40,6 +71,10 @@ class _Card:
   def __init__(self, suit, pip):
     """
     Card constructor.
+
+    In:
+    suit (string): One of Clubs, Diamonds, Hearts or Spades
+    pip (string): One of 1 - 10, Jack, Queen, King or Ace
     """
     
     self.suit = suit
--- lib/test_pack.py
+++ lib/test_pack.py
@@ -3,31 +3,69 @@ import pytest
 from pack import Pack, _Deck, _Card
 
 
-def test_deck_cards(deck):
+@pytest.mark.parametrize('card_holder, expected_cards', [
+  # Pack has expected cards
+  (Pack(), [_Card('Clubs', '2'), _Card('Spades', 'Ace')]),
+  # Deck has expected cards
+  (_Deck(), [_Card('Clubs', '2'), _Card('Spades', 'Ace')])
+])
+def test_cards(card_holder, expected_cards):
   """
   Deck is made with right cards.
   """
   
-  expected_cards = [_Card('Clubs', '2'), _Card('Spades', 'Ace')]
-  actual_cards = [deck.cards[0], deck.cards[len(deck.cards) - 1]]
+  actual_cards = [card_holder.cards[0], card_holder.cards[len(card_holder.cards) - 1]]
 
   for i in range(2):
     assert actual_cards[i].suit == expected_cards[i].suit
     assert actual_cards[i].pip == expected_cards[i].pip
 
 
-def test_deck_size(deck):
+@pytest.mark.parametrize('card_holder, expected_size', [
+  # Pack has 6 * 52 cards
+  (Pack(), Pack._DECKS * _Deck._CARDS),
+  # Deck has 52 cards
+  (_Deck(), _Deck._CARDS)
+])
+def test_cards_size(card_holder, expected_size):
   """
-  Deck is made with right number of cards.
+  Card holder is made with right number of cards.
   """
 
-  expected = 52
-  actual = len(deck.cards)
+  expected = expected_size
+  actual = len(card_holder.cards)
 
   assert actual == expected
 
 
+def test_pack_shuffle(pack):
+  """
+  Pack shuffles cards to random state.
+  """
+
+  def _random(start, end):
+    return start
+
+  pack.shuffle(_random)
+
+  expected_cards = [_Card('Spades', 'Ace'), _Card('Spades', 'King')]
+  actual_cards = [pack.cards[0], pack.cards[len(pack.cards) - 1]]
+
+  for i in range(2):
+    assert actual_cards[i].suit == expected_cards[i].suit
+    assert actual_cards[i].pip == expected_cards[i].pip
+
+
 # Fixtures
+
+
+@pytest.fixture
+def pack():
+  """
+  Instance of Pack.
+  """
+
+  return Pack()
 
 
 @pytest.fixture
