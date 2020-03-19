b560e3df3e848a3dcf26efb87b3ff78239943973
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-03-19 14:27:52 -0700

    Seat can take a card in deal added with tests

--- library/test_game.py
+++ library/test_game.py
@@ -1,6 +1,8 @@
 import pytest
 
 from game import Game
+from seat import Player
+from pack import Card
 
 
 @pytest.mark.parametrize('prints, prompts, responses', [
@@ -91,3 +93,186 @@ def test_bet(prints, prompts, responses):
 
   game = Game(_print = test_print, _prompt = test_prompt)
   game._bet(game.seats[0])
+
+
+@pytest.mark.parametrize('index, expected', [
+  # Expected successes
+  (0, "('Clubs', '2')"),
+  (3, "('*', '*')"),
+])
+def test_deal_card(index, expected):
+  """
+  Deal cards to seats.
+
+  In:
+  index (int): Which seat is being dealt a card.
+  expected (str): String representation of the hand of the seat.
+  """
+
+  game = Game()
+  game._deal(game.seats, index)
+  actual = str(game.seats[index % len(game.seats)].cards[0])
+
+  assert actual == expected
+
+
+def test_deal_totals_no_score_ace(init_deal_totals):
+  """
+  Player totals having no score and gets ace.
+
+  In:
+  init_deal_totals (dict): Initialized values for deal totals.
+  """
+
+  # Test specific values
+  game = init_deal_totals['game']
+  index = init_deal_totals['index']
+  expected = ['1', '11']
+
+  # Act on values
+  card = game.pack.hit(card_str = "('Spades', 'Ace')")
+  game._deal(game.seats, index, card)
+
+  # Check values
+  actual = game.seats[index].totals
+  assert actual == expected
+
+
+def test_deal_totals_no_score_not_ace(init_deal_totals):
+  """
+  Player totals having no score and gets not ace.
+
+  In:
+  init_deal_totals (dict): Initialized values for deal totals.
+  """
+
+  # Test specific values
+  game = init_deal_totals['game']
+  index = init_deal_totals['index']
+  expected = ['2']
+
+  # Act on values
+  card = game.pack.hit(card_str = "('Clubs', '2')")
+  game._deal(game.seats, index, card)
+
+  # Check values
+  actual = game.seats[index].totals
+  assert actual == expected
+
+
+def test_deal_totals_one_score_ace(init_deal_totals):
+  """
+  Player totals having one score and gets ace.
+
+  In:
+  init_deal_totals (dict): Initialized values for deal totals.
+  """
+
+  # Test specific values
+  game = init_deal_totals['game']
+  index = init_deal_totals['index']
+  expected = ['3', '13']
+
+  # Act on values
+  card = game.pack.hit(card_str = "('Clubs', '2')")
+  game._deal(game.seats, index, card)
+
+  card = game.pack.hit(card_str = "('Spades', 'Ace')")
+  game._deal(game.seats, index, card)
+
+  # Check values
+  actual = game.seats[index].totals
+  assert actual == expected
+
+
+def test_deal_totals_one_score_not_ace(init_deal_totals):
+  """
+  Players totals having one score and gets not ace.
+
+  In:
+  init_deals_totals (dict): Initialized values for deal totals.
+  """
+
+  # Test specific values
+  game = init_deal_totals['game']
+  index = init_deal_totals['index']
+  expected = ['5']
+
+  # Act on values
+  card = game.pack.hit(card_str = "('Clubs', '2')")
+  game._deal(game.seats, index, card)
+
+  card = game.pack.hit(card_str = "('Hearts', '3')")
+  game._deal(game.seats, index, card)
+
+  # Check values
+  actual = game.seats[index].totals
+  assert actual == expected
+
+
+def test_deal_totals_two_scores_ace(init_deal_totals):
+  """
+  Player totals having two scores and gets ace.
+
+  In:
+  init_deals_totals (dict): Initialized values for deal totals.
+  """
+
+  # Test specific values
+  game = init_deal_totals['game']
+  index = init_deal_totals['index']
+  expected = ['2', '22']
+
+  # Act on values
+  card = game.pack.hit(card_str = "('Spades', 'Ace')")
+  game._deal(game.seats, index, card)
+
+  card = game.pack.hit(card_str = "('Hearts', 'Ace')")
+  game._deal(game.seats, index, card)
+
+  # Check values
+  actual = game.seats[index].totals
+  assert actual == expected
+
+
+def test_deal_totals_two_scores_not_ace(init_deal_totals):
+  """
+  Player tools having two scores and gets not ace.
+
+  In:
+  init_deals_totals (dict): Initialized values for deal totals.
+  """
+
+  # Test specific values
+  game = init_deal_totals['game']
+  index = init_deal_totals['index']
+  expected = ['4', '14']
+
+  # Act on values
+  card = game.pack.hit(card_str = "('Spades', 'Ace')")
+  game._deal(game.seats, index, card)
+
+  card = game.pack.hit(card_str = "('Hearts', '3')")
+  game._deal(game.seats, index, card)
+
+  # Check values
+  actual = game.seats[index].totals
+  assert actual == expected
+
+
+# Fixtures
+
+
+@pytest.fixture
+def init_deal_totals():
+  """
+  Initialized values for deal totals.
+  """
+
+  player = Player()
+  seats = [player]
+
+  return {
+    'game': Game(seats = seats),
+    'index': 0
+  }
