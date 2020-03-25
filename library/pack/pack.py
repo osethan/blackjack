5341c24730727ad539d7c0b77e36fb0c9befd040
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-03-25 15:16:21 -0700

    Seat moved to own folder

--- library/pack/pack.py
+++ library/pack/pack.py
@@ -58,30 +58,6 @@ class Pack:
     return self.cards.pop(0)
 
 
-# class _Deck:
-#   """
-#   Standard 52 card deck.
-#   """
-
-#   CardS = 52
-
-
-#   def __init__(self):
-#     """
-#     Deck constructor.
-#     """
-    
-#     self.cards = []
-
-#     suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-#     pips = [str(d) for d in list(range(2, 11))] + ['Jack', 'Queen', 'King', 'Ace']
-
-#     for suit in suits:
-#       for pip in pips:
-#         card = Card(suit, pip)
-#         self.cards += [card]
-
-
 class Card:
   """
   A card of a standard 52 card deck.
