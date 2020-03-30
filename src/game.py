dff98d4501599aef1a7b46ba0a79cdc99afef3cc
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-03-29 19:34:26 -0700

    Game bet coded and tested

--- src/game.py
+++ src/game.py
@@ -1,3 +1,5 @@
+import re
+
 from src.seat import Dealer, Player
 from src.pack import Pack
 
@@ -20,6 +22,42 @@ class Game:
     self.__pack = pack
     self.print = _print
     self.input = _input
+
+
+  def get_dealer(self):
+    """
+    Getter.
+
+    Out:
+    (Dealer): A game's dealer.
+    """
+
+    return self.__dealer
+
+  
+  def get_player(self):
+    """
+    Getter.
+
+    Out:
+    (Player): A game's player.
+    """
+
+    return self.__player
+
+
+  def get_pack(self):
+    """
+    Getter.
+
+    Out:
+    (Pack): A game's pack.
+    """
+
+    return self.__pack
+
+
+  # Instance methods
 
 
   def welcome(self):
@@ -50,6 +88,39 @@ class Game:
         return 1
 
 
+  def bet(self):
+    """
+    A player makes a bet.
+
+    Status codes:
+    0) Ok
+    1) Exit
+
+    Out:
+    (int): Status code.
+    """
+
+    while True:
+      self.print(f'Your purse has {self.get_player().get_purse().get_size()} chips. What is your bet?')
+      res = self.input()
+
+      if res == 'q':
+        return 1
+
+      pattern = r'^\d+$'
+      if not re.match(pattern, res):
+        self.print('Can only bet integer chips')
+        continue
+
+      res = int(res)
+      if res < 2 or res > 500 or res > self.get_player().get_purse().get_size():
+        self.print('Can only bet between 2 - 500 chips and no more than purse')
+        continue
+
+      self.get_player().make_bet(res)
+      return 0
+
+
   def exit(self):
     """
     Exit Blackjack.
@@ -62,5 +133,12 @@ if __name__ == "__main__":
   game = Game()
   
   status = game.welcome()
-  if status:
+  if status == 1:
     game.exit()
+  else:
+
+    status = game.bet()
+    if status == 1:
+      game.exit()
+    else:
+      pass
--- tests/test_game.py
+++ tests/test_game.py
@@ -70,3 +70,81 @@ def test_game_welcome(prints, prompts, inputs):
 
   game = Game(test_print, test_prompt)
   game.welcome()
+
+
+@pytest.mark.parametrize('prints, prompts, inputs, purse', [
+  # Expected successes
+  # A player makes a bet
+  ([
+    'Your purse has 200 chips. What is your bet?'
+  ], [], [
+    '100'
+  ], None),
+  # Expected failures
+  # A player doesn't bet a number
+  ([
+    'Your purse has 200 chips. What is your bet?',
+    'Can only bet integer chips',
+    'Your purse has 200 chips. What is your bet?'
+  ], [], [
+    'Blackjack',
+    '100'
+  ], None),
+  # A player doesn't make an integer bet
+  ([
+    'Your purse has 200 chips. What is your bet?',
+    'Can only bet integer chips',
+    'Your purse has 200 chips. What is your bet?'
+  ], [], [
+    '50.5',
+    '100'
+  ], None),
+  # A player bet under 2 chips
+  ([
+    'Your purse has 200 chips. What is your bet?',
+    'Can only bet between 2 - 500 chips and no more than purse',
+    'Your purse has 200 chips. What is your bet?'
+  ], [], [
+    '1',
+    '100'
+  ], None),
+  # A player bets over purse
+  ([
+    'Your purse has 200 chips. What is your bet?',
+    'Can only bet between 2 - 500 chips and no more than purse',
+    'Your purse has 200 chips. What is your bet?'
+  ], [], [
+    '250',
+    '100'
+  ], None),
+  # A player bets over 500 chips
+  ([
+    'Your purse has 600 chips. What is your bet?',
+    'Can only bet between 2 - 500 chips and no more than purse',
+    'Your purse has 600 chips. What is your bet?'
+  ], [], [
+    '550',
+    '100'
+  ], 600)
+])
+def test_game_bet(prints, prompts, inputs, purse):
+  """
+  A player can make a bet.
+  """
+
+  def test_print(message):
+    _print = prints.pop(0)
+    assert message == _print
+
+  def test_prompt(message = ''):
+    if len(prompts) > 0:
+      _prompt = prompts.pop(0)
+      assert message == _prompt
+    return inputs.pop(0)
+
+  game = Game(test_print, test_prompt)
+
+  if purse:
+    game.get_player().get_purse().set_size(purse)
+
+  game.bet()
